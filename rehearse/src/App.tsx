61a4c909a2311703031948b8980e060c721755d8
stevenspicher <71337603+stevenspicher@users.noreply.github.com>
2023-02-06 08:25:06 -0700

    installed material, experimented

--- rehearse/src/App.tsx
+++ rehearse/src/App.tsx
@@ -1,18 +1,37 @@
 import './App.css'
+import {Box, TextField} from "@mui/material";
+import React from "react";
+import SignUp from "./components/Auditions/screens/AuditionForm";
+import LabelBottomNavigation from "./components/Navigation/screens/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Suite Surrender Rehearsal App</h1>
-      <div className="card">
-        <p>
-          In progress
-        </p>
-      </div>
-      <p className="read-the-docs">
-        calendar, blocking, notes, etc
-      </p>
+      <>
+    <div >
+    {/*  <h1>Suite Surrender Rehearsal App</h1>*/}
+      {/*<div className="card">*/}
+      {/*    <TextField id="outlined-basic" label="First Name" variant="outlined"/>*/}
+      {/*    <TextField id="outlined-basic" label="Last Name" variant="outlined"/>*/}
+      {/*</div>*/}
+
+      {/*  <Box*/}
+      {/*      sx={{*/}
+      {/*          width: 300,*/}
+      {/*          height: 300,*/}
+      {/*          backgroundColor: 'primary.dark',*/}
+      {/*          '&:hover': {*/}
+      {/*              backgroundColor: 'primary.main',*/}
+      {/*              opacity: [0.9, 0.8, 0.7],*/}
+      {/*          },*/}
+      {/*      }}*/}
+      {/*  />*/}
+      {/*<p className="read-the-docs">*/}
+      {/*  calendar, blocking, notes, etc*/}
+      {/*</p>*/}
+        <SignUp/>
+        <LabelBottomNavigation/>
     </div>
+      </>
   )
 }
 
